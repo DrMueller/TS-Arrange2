6b5b9014192cbc62d49b380347c3a42c95b52e61
Matthias Mueller <matthiasm@live.de>
2017-10-15 11:50:54 +0200

    Init

--- test/file-arrangement/configuration/services/configuration.service.test.ts
+++
@@ -1,9 +0,0 @@
-// import { ConfigurationService } from '../../../../src/file-arrangement/configuration';
-
-// describe('In the Configuration Service', () => {
-//   describe('The readConfigurationCollection method', () => {
-//     it('Should return all configuration elements', () => {
-//       // pending('Learning how to mock the VS Code Document');
-//     });
-//   });
-// });
