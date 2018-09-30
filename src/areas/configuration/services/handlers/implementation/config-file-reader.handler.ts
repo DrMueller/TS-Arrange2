9f439b5c8aef7529c0eaa7e3f042b1c8af1bf43c
Matthias Mueller <matthiasm@live.de>
2018-09-30 16:53:02 +0200

    WIP

--- src/areas/configuration/services/handlers/implementation/config-file-reader.handler.ts
+++ src/areas/configuration/services/handlers/implementation/config-file-reader.handler.ts
@@ -6,7 +6,7 @@ import * as vscode from 'vscode';
 @injectable()
 export class ConfigFileReader implements IConfigFileReader {
   public async readTsConfigAsync(): Promise<string> {
-    const tsLintFiles = await vscode.workspace.findFiles('tslint.json');
+    const tsLintFiles = await vscode.workspace.findFiles('**/tslint.json');
     const firstTsLint = tsLintFiles[0];
 
     const textDocument = await vscode.workspace.openTextDocument(firstTsLint);
